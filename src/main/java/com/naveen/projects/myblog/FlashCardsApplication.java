0e7faba13dc24a93cc0a718446ea63cf306a984a
Naveen <batchunaveen6@gmail.com>
2019-07-28 23:15:44 -0400

    adding repo

--- src/main/java/com/naveen/projects/myblog/FlashCardsApplication.java
+++ src/main/java/com/naveen/projects/myblog/FlashCardsApplication.java
@@ -1,13 +1,15 @@
 package com.naveen.projects.myblog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyblogApplication {
+@EnableAutoConfiguration
+public class FlashCardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyblogApplication.class, args);
+		SpringApplication.run(FlashCardApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/naveen/projects/myblog/domain/FlashCardsRepository.java
@@ -0,0 +1,4 @@
+package com.naveen.projects.myblog.domain;
+
+public interface FlashCardsRepository {
+}
